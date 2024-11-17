f06e47a6b3d6dd09ce12cdf42a0302671c8f235c
adir <adir@gmail.com>
2024-11-17 19:19:46 +0000

    Resolve comments module and create template file request.rest

--- src/models/commentModel.js
+++ src/models/commentModel.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 
 const CommentSchema = new mongoose.Schema({
-    _id: { type: Number, required: true },
     postId: { type: mongoose.Schema.Types.ObjectId, ref: 'Post', required: true },
     content: { type: String, required: true },
     sender: { type: String, required: true },

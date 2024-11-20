0810b464c62ac7de1835593f1a228732d0032cb0
adir <adir@gmail.com>
2024-11-20 16:45:33 +0000

    support swagger api

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express = require('express');
+
+const swaggerUi = require('swagger-ui-express');
+const swaggerDoc = require('./swagger.json');
+
 const app = express();
+
 const connectDB = require('./src/config/database');
 
 const postRoutes = require('./src/routes/postRoutes');
@@ -10,6 +15,10 @@ const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(express.json());
+
+// Swagger API
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
+
 
 // Routes
 app.use('/posts', postRoutes);

fe4dea360ee7a6cce8cc65fb9c14107a2670ed2d
adir <adir@gmail.com>
2024-11-20 17:11:45 +0000

    jwt implementation

--- app.js
+++ app.js
@@ -9,8 +9,9 @@ const connectDB = require('./src/config/database');
 
 const postRoutes = require('./src/routes/postRoutes');
 const commentRoutes = require('./src/routes/commentRoutes');
+const authRoutes = require('./src/routes/authRoutes');
 
-
+require('dotenv').config();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
@@ -23,6 +24,8 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 // Routes
 app.use('/posts', postRoutes);
 app.use('/comments', commentRoutes);
+app.use('/api/auth', authRoutes);
+
 
 
 // Database Connection
--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
 
+const mongoUrl = process.env.MONGO_URI ?? 'mongodb://localhost:27017/blog-api';
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb://localhost:27017/blog-api');
-        console.log('Database connected successfully at mongodb://localhost:27017/blog-api');
+        await mongoose.connect(mongoUrl);
+        console.log(`Database connected successfully at ${mongoUrl}`);
     } catch (err) {
         console.error('Database connection error: ', err);
         process.exit(1);
